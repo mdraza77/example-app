d490d2638388579f96831a6f311e8b7d01cef698
Md Raza <mdraza8397@gmail.com>
2025-07-29 23:57:07 +0530

    second commit of laravel learning

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <h1>Welcome, {{ $name ?? "Guest"}}</h1>
-    <!-- <h1>{{ date('d-m-y')}}</h1>
+
+<body style="text-align: center;">
+    <h1>Welcome, {{ $name ?? 'Guest' }}</h1>
+    <!-- <h1>{{ date('d-m-y') }}</h1>
     Welcome, {{ $demo }}
     <h1>Welcome, {!! $demo !!}</h1>
-    @if($name == ""){
-        {{"Empty"}}
+    @if ($name == '')
+{
+        {{ 'Empty' }}
     }
-    @elseif($name == "raza") {
-        {{"Name is raza"}}
+@elseif($name == 'raza')
+{
+        {{ 'Name is raza' }}
     }
-    @else {
-        {{"Name is not raza"}}
+@else
+{
+        {{ 'Name is not raza' }}
     }
-    @endif -->
-    <!-- @unless ($name == "raza")
-        <h1>Name is not raza</h1>
-    @endunless -->
+@endif -->
+    <!-- @unless ($name == 'raza')
+    <h1>Name is not raza</h1>
+@endunless -->
 
-    @isset($name)
-        <h1>{{$name}} Name is set</h1>
-    @endisset
-    
+    <!-- @isset($name)
+    <h1>{{ $name }} Name is set</h1>
+@endisset -->
+
+    @for ($i = 0; $i < 10; $i++)
+        <h2>
+            {{ $i }}
+        </h2>
+    @endfor
+
 </body>
+
 </html>
