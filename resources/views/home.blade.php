e00ecb3f680194d8ced363e14b5fb11acd5969b0
Md Raza <mdraza8397@gmail.com>
2025-07-30 05:56:37 +0530

    second commit with conditional statements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,41 +4,333 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home - Blade - PHP</title>
+    @vite(['resources/css/app.css'])
 </head>
 
-<body style="text-align: center;">
+<body class="bg-gray-900 text-white">
     <h1>Welcome, {{ $name ?? 'Guest' }}</h1>
-    <!-- <h1>{{ date('d-m-y') }}</h1>
+
+    {{-- <h1>{{ date('d-m-y') }}</h1>
     Welcome, {{ $demo }}
     <h1>Welcome, {!! $demo !!}</h1>
+
+    @if-else
     @if ($name == '')
-{
+        {
         {{ 'Empty' }}
-    }
-@elseif($name == 'raza')
-{
+        }
+    @elseif($name == 'raza')
+        {
         {{ 'Name is raza' }}
-    }
-@else
-{
+        }
+    @else
+        {
         {{ 'Name is not raza' }}
-    }
-@endif -->
-    <!-- @unless ($name == 'raza')
-    <h1>Name is not raza</h1>
-@endunless -->
+        }
+    @endif --}}
 
-    <!-- @isset($name)
-    <h1>{{ $name }} Name is set</h1>
-@endisset -->
+    {{-- @unless --}}
+    {{-- @unless ($name == 'raza')
+        <h1>Name is not raza</h1>
+    @endunless --}}
 
-    @for ($i = 0; $i < 10; $i++)
+    {{-- @isset --}}
+    {{-- @isset($name)
+        <h1>{{ $name }} Name is set</h1>
+    @endisset --}}
+
+
+    {{-- @for loop --}}
+    {{-- @for ($i = 0; $i < 10; $i++)
         <h2>
             {{ $i }}
         </h2>
-    @endfor
+    @endfor --}}
 
+    {{-- @while loop --}}
+    {{-- @php
+    $i = 1;
+    @endphp
+    @while ($i <= 10)
+        <h2>{{$i}}</h2>
+        @php $i++; @endphp
+    @endwhile --}}
+
+    {{-- foreach loop --}}
+
+    {{-- foreach loop with @break and @continu --}}
+    @php
+        // $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        $countries = [
+            'Afghanistan',
+            'Albania',
+            'Algeria',
+            'American Samoa',
+            'Andorra',
+            'Angola',
+            'Anguilla',
+            'Antarctica',
+            'Antigua and Barbuda',
+            'Argentina',
+            'Armenia',
+            'Aruba',
+            'Australia',
+            'Austria',
+            'Azerbaijan',
+            'Bahamas',
+            'Bahrain',
+            'Bangladesh',
+            'Barbados',
+            'Belarus',
+            'Belgium',
+            'Belize',
+            'Benin',
+            'Bermuda',
+            'Bhutan',
+            'Bolivia',
+            'Bosnia and Herzegowina',
+            'Botswana',
+            'Bouvet Island',
+            'Brazil',
+            'British Indian Ocean Territory',
+            'Brunei Darussalam',
+            'Bulgaria',
+            'Burkina Faso',
+            'Burundi',
+            'Cambodia',
+            'Cameroon',
+            'Canada',
+            'Cape Verde',
+            'Cayman Islands',
+            'Central African Republic',
+            'Chad',
+            'Chile',
+            'China',
+            'Christmas Island',
+            'Cocos (Keeling) Islands',
+            'Colombia',
+            'Comoros',
+            'Congo',
+            'Congo, the Democratic Republic of the',
+            'Cook Islands',
+            'Costa Rica',
+            "Cote d'Ivoire",
+            'Croatia (Hrvatska)',
+            'Cuba',
+            'Cyprus',
+            'Czech Republic',
+            'Denmark',
+            'Djibouti',
+            'Dominica',
+            'Dominican Republic',
+            'East Timor',
+            'Ecuador',
+            'Egypt',
+            'El Salvador',
+            'Equatorial Guinea',
+            'Eritrea',
+            'Estonia',
+            'Ethiopia',
+            'Falkland Islands (Malvinas)',
+            'Faroe Islands',
+            'Fiji',
+            'Finland',
+            'France',
+            'France Metropolitan',
+            'French Guiana',
+            'French Polynesia',
+            'French Southern Territories',
+            'Gabon',
+            'Gambia',
+            'Georgia',
+            'Germany',
+            'Ghana',
+            'Gibraltar',
+            'Greece',
+            'Greenland',
+            'Grenada',
+            'Guadeloupe',
+            'Guam',
+            'Guatemala',
+            'Guinea',
+            'Guinea-Bissau',
+            'Guyana',
+            'Haiti',
+            'Heard and Mc Donald Islands',
+            'Holy See (Vatican City State)',
+            'Honduras',
+            'Hong Kong',
+            'Hungary',
+            'Iceland',
+            'India',
+            'Indonesia',
+            'Iran (Islamic Republic of)',
+            'Iraq',
+            'Ireland',
+            'Israel',
+            'Italy',
+            'Jamaica',
+            'Japan',
+            'Jordan',
+            'Kazakhstan',
+            'Kenya',
+            'Kiribati',
+            "Korea, Democratic People's Republic of",
+            'Korea, Republic of',
+            'Kuwait',
+            'Kyrgyzstan',
+            "Lao, People's Democratic Republic",
+            'Latvia',
+            'Lebanon',
+            'Lesotho',
+            'Liberia',
+            'Libyan Arab Jamahiriya',
+            'Liechtenstein',
+            'Lithuania',
+            'Luxembourg',
+            'Macau',
+            'Macedonia, The Former Yugoslav Republic of',
+            'Madagascar',
+            'Malawi',
+            'Malaysia',
+            'Maldives',
+            'Mali',
+            'Malta',
+            'Marshall Islands',
+            'Martinique',
+            'Mauritania',
+            'Mauritius',
+            'Mayotte',
+            'Mexico',
+            'Micronesia, Federated States of',
+            'Moldova, Republic of',
+            'Monaco',
+            'Mongolia',
+            'Montserrat',
+            'Morocco',
+            'Mozambique',
+            'Myanmar',
+            'Namibia',
+            'Nauru',
+            'Nepal',
+            'Netherlands',
+            'Netherlands Antilles',
+            'New Caledonia',
+            'New Zealand',
+            'Nicaragua',
+            'Niger',
+            'Nigeria',
+            'Niue',
+            'Norfolk Island',
+            'Northern Mariana Islands',
+            'Norway',
+            'Oman',
+            'Pakistan',
+            'Palau',
+            'Panama',
+            'Papua New Guinea',
+            'Paraguay',
+            'Peru',
+            'Philippines',
+            'Pitcairn',
+            'Poland',
+            'Portugal',
+            'Puerto Rico',
+            'Qatar',
+            'Reunion',
+            'Romania',
+            'Russian Federation',
+            'Rwanda',
+            'Saint Kitts and Nevis',
+            'Saint Lucia',
+            'Saint Vincent and the Grenadines',
+            'Samoa',
+            'San Marino',
+            'Sao Tome and Principe',
+            'Saudi Arabia',
+            'Senegal',
+            'Seychelles',
+            'Sierra Leone',
+            'Singapore',
+            'Slovakia (Slovak Republic)',
+            'Slovenia',
+            'Solomon Islands',
+            'Somalia',
+            'South Africa',
+            'South Georgia and the South Sandwich Islands',
+            'Spain',
+            'Sri Lanka',
+            'St. Helena',
+            'St. Pierre and Miquelon',
+            'Sudan',
+            'Suriname',
+            'Svalbard and Jan Mayen Islands',
+            'Swaziland',
+            'Sweden',
+            'Switzerland',
+            'Syrian Arab Republic',
+            'Taiwan, Province of China',
+            'Tajikistan',
+            'Tanzania, United Republic of',
+            'Thailand',
+            'Togo',
+            'Tokelau',
+            'Tonga',
+            'Trinidad and Tobago',
+            'Tunisia',
+            'Turkey',
+            'Turkmenistan',
+            'Turks and Caicos Islands',
+            'Tuvalu',
+            'Uganda',
+            'Ukraine',
+            'United Arab Emirates',
+            'United Kingdom',
+            'United States',
+            'United States Minor Outlying Islands',
+            'Uruguay',
+            'Uzbekistan',
+            'Vanuatu',
+            'Venezuela',
+            'Vietnam',
+            'Virgin Islands (British)',
+            'Virgin Islands (U.S.)',
+            'Wallis and Futuna Islands',
+            'Western Sahara',
+            'Yemen',
+            'Yugoslavia',
+            'Zambia',
+            'Zimbabwe',
+        ];
+
+        // echo "<pre>";
+        // print_r($countries);
+
+    @endphp
+
+    {{-- <select
+        class="bg-gray-800 text-white text-lg border border-gray-600 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent shadow-md w-full max-w-md mx-auto mt-8">
+        @foreach ($countries as $key => $country)
+            <option value="{{$key}}" class="text-black text-lg">{{ $country }}</option>
+        @endforeach
+    </select> --}}
+
+    {{-- @for loop with @continue statement--}}
+    {{-- @for ($i = 0; $i <= 10; $i++)
+        @if ($i == 3)
+            @continue
+        @endif
+        {{ $i }}
+    @endfor --}}
+
+    {{-- @for loop with @break statement--}}
+    @for ($j = 1; $j <= 10; $j++)
+        @if ($j == 5)
+            @break
+        @endif
+        {{ $j }}
+    @endfor
 </body>
 
 </html>
